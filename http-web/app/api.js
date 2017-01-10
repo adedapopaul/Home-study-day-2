200e573e9012af1198e1a91d28e2194d30e2f127
adedapopaul <adedapopaul@yahoo.com>
2017-01-10 17:53:02 +0100

    Display NASA satellite image on browser

--- http-web/app/api.js
+++ http-web/app/api.js
@@ -5,11 +5,13 @@
 //TO CHECK THE DESCRIPTION, TYPE 'description';
 //TO CHECK THE DATE OF THE FEED, TYPE 'date'
 //TO CHECK THE AUTHOR OF THE FEED, TYPE 'author'
+//TO IMAGE OF THE FEED, TYPE 'image'
 // TO LIST AVAILABLE COMMANDS, TYPE 'commands'
 
 
 
 var request = require('request');
+var open = require('open');
 
 request('https://api.nasa.gov/planetary/apod?api_key=SliDHtb2JbBGZYlTiwuHz4JKAHvdgxwV5660aXmo', function (error, response, body) {
    if (!error && response.statusCode == 200) {
@@ -22,11 +24,12 @@ request('https://api.nasa.gov/planetary/apod?api_key=SliDHtb2JbBGZYlTiwuHz4JKAHv
     const io = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
-        prompt: 'NASA Neo-feed>'
+        prompt: 'NASA Neo-feed>',
+        prompt: console.log('Welcome to NASA Neo-Feed')
     });
 
     io.on('line', function(line) {
-        //createnote <note one>
+
         var words = line.trim();
         command = words;
 
@@ -49,7 +52,8 @@ request('https://api.nasa.gov/planetary/apod?api_key=SliDHtb2JbBGZYlTiwuHz4JKAHv
 
                 break;
             case 'image':
-               showResponse(result);
+              console.log("please wait while the image loads on your default browser");
+               open(result.hdurl);
 
                 break;
 
@@ -73,9 +77,9 @@ request('https://api.nasa.gov/planetary/apod?api_key=SliDHtb2JbBGZYlTiwuHz4JKAHv
   function listComands(){
         console.log("date : display the date of the feed from NASA");
         console.log("description : display NASA feed explanation ");
-        // console.log("image : display the inage of the feed");
+        console.log("image : display the inage of the feed");
         console.log("title : display the title of the feed");
-        // console.log("next : display the next feed");
+        
   };
 
 
